135cae7457526da6ef4787c039e49f753d867254
Ali <ma.essa.pk@gmail.com>
2017-08-16 09:02:19 +0100

    Apply CSS transition effects to the HTML elements in the Vue Component

--- resources/assets/js/components/CarModelsList.vue
+++ resources/assets/js/components/CarModelsList.vue
@@ -2,45 +2,51 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8">
-                <div class="alert alert-success" v-if="message">
-                    {{message}}
-                </div>
+                <transition name="fade">
+                    <div class="alert alert-success" v-if="message">
+                        {{message}}
+                    </div>
+                </transition>
 
                 <div class="alert alert-danger" v-if="isError">
                     {{errorMessage}}
                 </div>
-                <div class="panel panel-primary" v-for="model in carModelsData">
-                    <div class="panel-heading">
-                        <h1 class="panel-title">Model Id: {{model.modelId}}</h1>
-                    </div>
-                    <div class="panel-body">    
-                        <ul>
-                            <li>Co2 Emissions: {{model.co2Emmissions}}</li>
-                            <li>Engine Power: {{model.enginePower}}</li>
-                            <li>Engine Size: {{model.engineSize}}</li>
-                            <li>Fuel Type: {{model.fuelType}}</li>
-                        </ul>
-                        <table class="table">
-                            <tr>
-                                <th>Car Id</th>
-                                <th>Mileage</th>
-                                <th>Registration</th>
-                                <th>Owners</th>
-                                <th>Transmission</th>
-                                <th>Price</th>
-                            </tr>
 
-                            <tr v-for="car in model.cars">
-                                <td>{{car.id}}</td>
-                                <td>{{car.mileage}}</td>
-                                <td>{{car.registration}}</td>
-                                <td>{{car.owners}}</td>
-                                <td>{{car.transmission}}</td>
-                                <td>{{car.price}}</td>
-                            </tr>                        
-                        </table>
-                    </div>                          
-                </div>
+                <transition-group name="bounce" tag="div">                
+                    <div class="panel panel-primary" v-for="model in carModelsData" :key="model.modelId">
+                        <div class="panel-heading">
+                            <h1 class="panel-title">Model Id: {{model.modelId}}</h1>
+                        </div>
+                        <div class="panel-body">    
+                            <ul>
+                                <li>Co2 Emissions: {{model.co2Emmissions}}</li>
+                                <li>Engine Power: {{model.enginePower}}</li>
+                                <li>Engine Size: {{model.engineSize}}</li>
+                                <li>Fuel Type: {{model.fuelType}}</li>
+                            </ul>
+                            <table class="table">
+                                <tr>
+                                    <th>Car Id</th>
+                                    <th>Mileage</th>
+                                    <th>Registration</th>
+                                    <th>Owners</th>
+                                    <th>Transmission</th>
+                                    <th>Price</th>
+                                </tr>
+
+                                <tr v-for="car in model.cars">
+                                    <td>{{car.id}}</td>
+                                    <td>{{car.mileage}}</td>
+                                    <td>{{car.registration}}</td>
+                                    <td>{{car.owners}}</td>
+                                    <td>{{car.transmission}}</td>
+                                    <td>{{car.price}}</td>
+                                </tr>                        
+                            </table>
+                        </div>                          
+                    </div>
+                </transition-group>
+
             </div>
             <div class="col-md-4 well">
                 <h3>Apply Filters</h3>
@@ -113,3 +119,34 @@
         }
     }
 </script>
+<style>
+    .fade-enter-active, .fade-leave-active {
+      transition: opacity 1s;
+    }
+    .fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
+      opacity: 0;
+    }
+
+    .bounce-enter-active {
+      animation: bounce-in .5s;
+    }
+    .bounce-leave-active {
+      animation: bounce-in .5s reverse;
+    }
+
+    @keyframes bounce-in {
+      0% {
+        transform: scale(0);
+      }
+      50% {
+        transform: scale(1.5);
+      }
+      100% {
+        transform: scale(1);
+      }
+    }    
+
+    .bounce-move {
+        transition: transform 1s;
+    }
+</style>
